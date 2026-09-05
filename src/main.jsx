4a989f15635668c38bd967050c404e88ec66716b
kaviya <sangeethakavi07@gmail.com>
2026-09-05 15:56:50 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/viyazham-publication">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
